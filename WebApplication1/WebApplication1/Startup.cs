54fff65bb11da6fa60252a61679907d4d2c19f01
test <test@test.com>
2019-12-12 15:12:08 +0800

    s

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -42,7 +42,7 @@ namespace WebApplication1
            // hao
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         } 
-        //nihao 
+        //ss
 //hehe 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
